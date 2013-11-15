1f1dd5cedeb58794ae0f8b0b18bfa3ffd00713cd
rtyqwe <ryanwmchan@icloud.com>
2013-11-15 13:15:15 -0800

    A2 implemented

--- src/edu/ucla/cs/cs144/SearchServlet.java
+++ src/edu/ucla/cs/cs144/SearchServlet.java
@@ -1,6 +1,12 @@
 package edu.ucla.cs.cs144;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,9 +16,29 @@ import javax.servlet.http.HttpServletResponse;
 public class SearchServlet extends HttpServlet implements Servlet {
        
     public SearchServlet() {}
-
+    
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // your codes here
+    	String query = request.getParameter("q");
+    	int numResultsToSkip = Integer.parseInt(request.getParameter("numResultsToSkip"));
+    	int numResultsToReturn = Integer.parseInt(request.getParameter("numResultsToReturn"));
+    	
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        out.println("<html>" +
+        		"<head></head>"+
+        		"<body>");
+        
+        SearchResult[] results = AuctionSearchClient.basicSearch(query, numResultsToSkip, numResultsToReturn);
+        
+        String tmpItemID = null;
+        String tmpItemName = null;
+        for(int i = 0; i < results.length; i++){
+        	tmpItemID = results[i].getItemId();
+        	tmpItemName = results[i].getName();
+        	out.println("<a href=\"item?id=" + tmpItemID + "\">" + tmpItemID + "</a>    " + tmpItemName+"<br>");
+        }
+        out.println("</body></html>");
     }
 }
